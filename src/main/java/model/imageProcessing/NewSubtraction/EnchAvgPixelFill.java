579b6b8d83216573155d74bc9a79befd99f5d466
Laimi <Laimil2012@gmail.com>
2017-07-13 16:11:53 +0300

    Radius can be changed now and used as new parameter. Also counter can be used as additional parameter on UI. Probably the best values for radius between 2 and 5 included.

--- src/main/java/model/imageProcessing/NewSubtraction/EnchAvgPixelFill.java
+++ src/main/java/model/imageProcessing/NewSubtraction/EnchAvgPixelFill.java
@@ -8,12 +8,17 @@ import model.imageProcessing.imageTypes.ImageBin;
 public class EnchAvgPixelFill extends AvgPixelFill {
     public boolean getSummPlus(int[][] a, int x, int y){
         int count = 0;
-        int r=2;
+        int r=3;
        for (int i = x-r; i < x+r; i++) {
            for (int j = y-r; j < y+r; j++) {
+//               if(x<=0||y<=0){ j+=r; i+=r; x+=r; y+=r; }
+              if(x+r>=a.length||y+r>=a[0].length)break;
+               if(x-r<=0||y-r<=0)break;
                if(a[x][y]==a[i][j])count++;
                if(x==i&&y==j)count--;
            }
+           if(x+r>=a.length||y+r>=a[0].length)break;
+           if(x-r<=0||y-r<=0)break;
        }
 
         if(count>=3){
@@ -26,8 +31,8 @@ public class EnchAvgPixelFill extends AvgPixelFill {
         int width = originalImage.length;
         int height = originalImage[0].length;
         int[][] resultingImage = new int[width][height];
-        for(int x=2; x<image.getWidth()-2; x++){
-            for(int y=2; y<image.getHeight()-2; y++){
+        for(int x=0; x<image.getWidth(); x++){
+            for(int y=0; y<image.getHeight(); y++){
                 resultingImage[x][y]=originalImage[x][y];
                 if(getSummPlus(originalImage, x, y)){
                     resultingImage[x][y]=0;
